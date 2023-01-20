dc730debe427e5e35c17c7033dd4dbb8958d0758
Vincent Ang <vinceajy93@gmail.com>
2023-01-20 10:59:33 +0800

    initial commit

---
+++ App.java
@@ -0,0 +1,29 @@
+package sg.edu.nus.iss;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public final class App {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Integer guessNum = rand.nextInt();
+
+        Integer myGuess = 0;
+
+        //using scanner
+        Scanner input = new Scanner(System.in);
+        while(myGuess!=guessNum){
+            myGuess = input.nextInt();
+            if(myGuess < guessNum){
+                System.out.println("The number you guess is lower.");
+            }else if(myGuess > guessNum){
+                System.out.println("The number you guess is higher.");
+            }
+            else{
+                System.out.println("You guess it right!");
+                input.close();
+                System.exit(0);
+            }
+        }
+    }
+}
